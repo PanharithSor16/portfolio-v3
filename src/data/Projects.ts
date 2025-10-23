2d9ad8d9f8e3e71bd460bc2becba719062c17b92
PanharithSor16 <panharithsor16@gmail.com>
2025-10-23 16:07:22 +0700

    portfolio v3

---
+++ src/data/Projects.ts
@@ -0,0 +1,17 @@
+import apiImage from "/src/assets/api.png";
+
+export const Projects = [
+  {
+    id: 1,  
+    title: "Portfolio Website",
+    description:
+      "A personal portfolio website to showcase my projects and skills.",
+    image: apiImage,
+    link: "",
+    github: ""
+
+  },
+  {
+    id: 2,
+    title: "jart Je"
+    } ]  
